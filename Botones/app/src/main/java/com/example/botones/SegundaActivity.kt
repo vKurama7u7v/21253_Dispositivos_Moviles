47dc50657c22356e065cc3525306509d9eba527c
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-01-30 18:59:10 -0600

    30 Jan 2023 06:59

--- Botones/app/src/main/java/com/example/botones/SegundaActivity.kt
+++ Botones/app/src/main/java/com/example/botones/SegundaActivity.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
-class SegundaActivity : AppCompatActivity() {
+class SegundaActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cambioIcono: Boolean = false
+    private lateinit var  imgLogo : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
@@ -16,16 +22,71 @@ class SegundaActivity : AppCompatActivity() {
 
         Log.d("DATOS:", "Datos recibidos con exito fueron frase $getFrase y edad $getEdad")
 
+        // GifLoader ==> Libreria para reproducir gifs
+
+
+        // Cambiar ImageView
+        imgLogo = findViewById<ImageView>(R.id.imgVLogo)
+        //var lel = false
+        /*imgLogo.setOnClickListener {
+            //if (lel){
+                //imgLogo.setImageResource(R.drawable.a)
+                //lel = true
+            //} else {
+                //imgLogo.setImageResource(R.drawable.iest)
+                //lel = false
+            //}
+
+            var arregloImagenes = arrayOf<Int>(R.drawable.a, R.drawable.iest)
+            if (cambioIcono) {
+                imgLogo.setImageResource(arregloImagenes[0])
+            } else {
+                imgLogo.setImageResource(arregloImagenes[1])
+            }
+
+            cambioIcono = !cambioIcono
+
+        }*/
+
+        // Cambiar de pantalla
         var btnAbrirPantalla = findViewById<Button>(R.id.btnPantalla)
-        btnAbrirPantalla.setOnClickListener{
+        /*btnAbrirPantalla.setOnClickListener{
             var i = Intent(this, MainActivity::class.java)
             startActivity(i)
             finish()
-        }
+        }*/
 
+        // Cerrar Pantalla
         var btnCerrarPantalla = findViewById<Button>(R.id.btnClose)
-        btnCerrarPantalla.setOnClickListener {
+        /*btnCerrarPantalla.setOnClickListener {
             finish()
+        }*/
+
+        btnCerrarPantalla.setOnClickListener(this)
+
+
+
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.btnClose -> finish()
+            R.id.btnPantalla -> {
+                var i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+            R.id.imgVLogo -> {
+                var arregloImagenes = arrayOf<Int>(R.drawable.a, R.drawable.iest)
+                if (cambioIcono) {
+                    imgLogo.setImageResource(arregloImagenes[0])
+                } else {
+                    imgLogo.setImageResource(arregloImagenes[1])
+                }
+
+                cambioIcono = !cambioIcono
+            }
+
         }
     }
 }
