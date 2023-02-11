419130d4092ee1a1dddb2df79ee02ef63f3d9393
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-10 18:38:39 -0600

    10 Feb 2023 18:38

--- AndroidWear/app/src/main/java/com/example/androidwear/MainActivity.kt
+++ AndroidWear/app/src/main/java/com/example/androidwear/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.androidwear
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.androidwear.databinding.ActivityRelojBinding
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityRelojBinding
     private var flag: Boolean = false
+    private var text: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +22,22 @@ class MainActivity : Activity() {
         binding = ActivityRelojBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txtSaludo.text = "Hola Amiguito :D"
+        binding.txtSaludo.text = "Saludo"
         binding.btnCambio.text = "Enviar :D"
 
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.misOpciones,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Relacionar Spinner con nuestro adapter
+        binding.spOpciones.adapter = adapter
+        binding.spOpciones.onItemSelectedListener = this
+
+        /*
         binding.btnCambio.setOnClickListener{
             if (flag){
                 binding.txtSaludo.text = "Demonios Gump"
@@ -26,7 +46,44 @@ class MainActivity : Activity() {
             }
 
             flag = !flag
-
         }
+        */
+
+        binding.btnCambio.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Atención")
+                .setMessage("Quiere enviar el saludo $text?")
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                    DialogInterface.OnClickListener{dialogInterface, i ->
+                        // Aqui el codigo
+                        binding.txtSaludo.text = text
+                    }
+                )
+                .setNegativeButton("Cancel",
+                    DialogInterface.OnClickListener{dialogInterface, i ->
+                        // Aqui va el codigo
+                        Toast.makeText(this, "Una lastima :c", Toast.LENGTH_SHORT).show()
+                    }
+                )
+                .show()
+        }
+    }
+
+
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        //TODO("Not yet implemented")
+        // p0 => Vista, en este caso el spinner
+        // p1 => Son las vistas que se van a repetidas (los items)
+        // p2 => Item Seleccionado (Posicion)
+        // p3 => id de la Vista
+
+        text = p0?.getItemAtPosition(p2).toString()
+        Toast.makeText(this, "Elegiste $text", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //TODO("Not yet implemented")
     }
 }
