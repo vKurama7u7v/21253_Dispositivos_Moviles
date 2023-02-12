2e901f82e547329c09b0e1042a08b8f716eecad6
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-12 16:26:33 -0600

    Update WearOSNumMayor

--- WearOSNumMayor/app/src/main/java/com/example/wearosnummayor/MainActivity.kt
+++ WearOSNumMayor/app/src/main/java/com/example/wearosnummayor/MainActivity.kt
@@ -5,16 +5,15 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Toast
 import com.example.wearosnummayor.databinding.ActivityMainBinding
 
-class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
+class MainActivity : Activity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-    private var numPrimero: String? = null
-    private var numSegundo: String? = null
+    private var btnCalcular : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,58 +21,68 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        inicializarVistas()
+        agregarEventos()
+    }
+
+    override fun onClick(p0: View?) {
+        //TODO("Not yet implemented")
+
+        // Obtener valor de los dos Spinners
+        val num1 = binding.spNumPrimary.selectedItem.toString().toInt()
+        val num2 = binding.spNumSecondary.selectedItem.toString().toInt()
+
+        // Validaciones ...
+            // Omitir ...
+
+        val methods = Methods()
+
+        when(p0?.id) {
+            R.id.btnCalcular -> {
+                // Al oprimir un botón muestre una alerta que mencione cual spinner
+                // contiene el mayor de los números seleccionado por el usuario.
+                val alert = AlertDialog.Builder(this)
+                alert.setTitle("Atención")
+                    .setMessage("¿Desea comparar los 2 valores?")
+                    .setCancelable(false)
+                    .setPositiveButton("Chi",
+                        DialogInterface.OnClickListener{ dialogInterface, i ->
+                            if (num1 == num2){
+                                Toast.makeText(this, "Los números son iguales", Toast.LENGTH_LONG).show()
+                            } else {
+                                val mayor = methods.comparar(num1, num2)
+                                Toast.makeText(this, "Número mayor = $mayor", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    )
+                    .setNegativeButton("Nel",
+                        DialogInterface.OnClickListener{ dialogInterface, i ->
+                            Toast.makeText(this, "Una lastima :c", Toast.LENGTH_LONG).show()
+                        }
+                    )
+                    .show()
+            }
+        }
+    }
+
+    private fun inicializarVistas() {
+        btnCalcular = binding.btnCalcular
+        // Array Adapter
         val adapter = ArrayAdapter.createFromResource(
             this,
             R.array.misNumeros,
             android.R.layout.simple_spinner_item
         )
-
-        val adapter2 = ArrayAdapter.createFromResource(
-            this,
-            R.array.misNumeros,
-            android.R.layout.simple_spinner_item
-        )
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         // Relacionar Spinner con nuestro adapter
         binding.spNumPrimary.adapter = adapter
-        binding.spNumPrimary.onItemSelectedListener = this
-
-        binding.spNumSecondary.adapter = adapter2
-        binding.spNumSecondary.onItemSelectedListener = this
-
-        binding.btnCambio.setOnClickListener {
-            val alert = AlertDialog.Builder(this)
-            alert.setTitle("Atención")
-                .setMessage("Quiere enviar el saludo $numPrimero?")
-                .setCancelable(false)
-                .setPositiveButton("OK",
-                    DialogInterface.OnClickListener{ dialogInterface, i ->
-                        // Aqui el codigo
-                        binding.txtSaludo.text = numPrimero
-                    }
-                )
-                .setNegativeButton("Cancel",
-                    DialogInterface.OnClickListener{ dialogInterface, i ->
-                        // Aqui va el codigo
-                        Toast.makeText(this, "Una lastima :c", Toast.LENGTH_SHORT).show()
-                    }
-                )
-                .show()
-        }
+        binding.spNumSecondary.adapter = adapter
     }
 
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        //TODO("Not yet implemented")
-        numPrimero = p0?.getItemAtPosition(p2).toString()
-        Toast.makeText(this, "Elegiste $numPrimero", Toast.LENGTH_LONG).show()
-    }
-
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {
-        //TODO("Not yet implemented")
+    private fun agregarEventos(){
+        btnCalcular?.setOnClickListener(this)
     }
 
 }
---
+++ WearOSNumMayor/app/src/main/java/com/example/wearosnummayor/Methods.kt
@@ -0,0 +1,8 @@
+package com.example.wearosnummayor
+
+class Methods {
+    fun comparar(num1: Int, num2: Int): Int{
+        if(num1 > num2) return num1
+        return num2
+    }
+}
