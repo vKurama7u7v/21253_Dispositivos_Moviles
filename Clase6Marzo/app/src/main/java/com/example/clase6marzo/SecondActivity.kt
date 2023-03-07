4a5d41e7f7dffb935c240c68ccff6566d4b8b551
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-06 18:50:30 -0600

    add App Clase6Marzo

---
+++ Clase6Marzo/app/src/main/java/com/example/clase6marzo/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clase6marzo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
