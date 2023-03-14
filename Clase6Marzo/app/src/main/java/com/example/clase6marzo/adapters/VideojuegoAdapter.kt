1bebb2fac42d207dd344a55fa10a307466d5e35f
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-13 18:04:50 -0600

    13 Marzo 2023 18:04

--- Clase6Marzo/app/src/main/java/com/example/clase6marzo/adapters/VideojuegoAdapter.kt
+++ Clase6Marzo/app/src/main/java/com/example/clase6marzo/adapters/VideojuegoAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.clase6marzo.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.clase6marzo.R
 import com.example.clase6marzo.models.Videojuego
 
-class VideojuegoAdapter(videojuegos: ArrayList<Videojuego>, context: Context) {
+class VideojuegoAdapter(videojuegos: ArrayList<Videojuego>, context: Context): RecyclerView.Adapter<VideojuegoAdapter.ContenedorDeVista>() {
     var innerVideojuegos: ArrayList<Videojuego> = videojuegos
     var innerContext: Context = context
 
@@ -31,5 +33,31 @@ class VideojuegoAdapter(videojuegos: ArrayList<Videojuego>, context: Context) {
             txtPrice = view.findViewById(R.id.txtPrice)
             btnBuy = view.findViewById(R.id.btnBuy)
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContenedorDeVista {
+        //TODO("Not yet implemented")
+        // => Retorna la vista de el componente de los videojuegos
+        // viewType = Se usa cuando se tienen adaptadores diferentes (ejemplo ads)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_second, parent, false)
+        return ContenedorDeVista(view)
+    }
+
+    override fun getItemCount(): Int {
+        //TODO("Not yet implemented")
+        // => Retorna el número de items
+        return innerVideojuegos.size
+    }
+
+    override fun onBindViewHolder(holder: ContenedorDeVista, position: Int) {
+        //TODO("Not yet implemented")
+        // => Se ejecuta cada vez que entra un nuevo item
+        // => Edita las vistas/labels de el layout (nombre, precio, imagen, etc)
+
+        val videojuego: Videojuego = innerVideojuegos.get(position)
+        holder.txtTitle.text = videojuego.nombre
+        holder.txtConsole.text = videojuego.consola
+        holder.txtPrice.text = videojuego.precio.toString()
+        holder.imageView.setImageResource(videojuego.imagen)
     }
 }
