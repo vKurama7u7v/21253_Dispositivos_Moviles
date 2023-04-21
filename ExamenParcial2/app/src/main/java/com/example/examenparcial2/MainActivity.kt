3e15af8502336a8d75976dbbd9b2f298fb68f939
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-04-21 18:05:11 -0500

    update Examen Segundo Parcial

--- ExamenParcial2/app/src/main/java/com/example/examenparcial2/MainActivity.kt
+++ ExamenParcial2/app/src/main/java/com/example/examenparcial2/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.examenparcial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var btnCat: Button? = null
+    private var btnProfile: Button? = null
+    private var btnSettings: Button? = null
+    private var btnClose: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inicializarVistas()
+        asignarEventos()
+    }
+
+    // Menu Icons
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_icons, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun inicializarVistas(){
+        btnCat = findViewById(R.id.optionCat)
+        btnProfile = findViewById(R.id.optionProfile)
+        btnSettings = findViewById(R.id.optionConfig)
+        btnClose = findViewById(R.id.optionClose)
+    }
+
+    private fun asignarEventos(){
+        btnCat?.setOnClickListener(this)
+        btnProfile?.setOnClickListener(this)
+        btnSettings?.setOnClickListener(this)
+        btnClose?.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        //TODO("Not yet implemented")
     }
 }
--- ExamenParcial2/app/src/main/java/com/example/examenparcial2/models/MenuOptions.kt
+++
@@ -1,7 +0,0 @@
-package com.example.examenparcial2.models
-
-data class MenuOptions(
-    var id: Int,
-    var title: String,
-    var img: Int
-)
